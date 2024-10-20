63816f478ce9b3ab8c60e923e3dbf336611a92df
Cameron <greayham@gmail.com>
2024-10-20 17:20:18 +1300

    Draw bounding boxes, only works for portrait atm

--- Scoreboard/CameraView.swift
+++ Scoreboard/CameraView.swift
@@ -11,7 +11,7 @@ import AVFoundation
 
 struct CameraView: View {
     @State var camera = CameraModel()
-    
+    @State var showHumanSegmentation = false
     var body: some View {
         CameraPreview(source: camera.previewSource)
         .task {
@@ -20,12 +20,50 @@ struct CameraView: View {
         .onReceive(camera.predictionTimer, perform: { _ in
             camera.canPredict = true
         })
-        .overlay(alignment: .top) {
-            HStack {
-                Text(camera.myLabel)
+//        .overlay(alignment: .center) {
+//            if showHumanSegmentation,
+//               let img = camera.myImage {
+//                
+//       
+//                Image(img, scale: 1.0, label: Text(""))
+//            }
+//        }
+//        .overlay(alignment: .topTrailing, content: {
+//            Button {
+//                showHumanSegmentation.toggle()
+//            } label: {
+//                Text("Segmentation")
+//            }
+//
+//        })
+        .overlay {
+            GeometryReader { geometry in
+                ForEach(camera.rects) { rectData in
+                    let adjustedRect = adjustRectForView(rect: rectData.rect, viewSize: geometry.size)
+                    Rectangle()
+                        .stroke(Color.red, lineWidth: 2)
+                        .frame(width: adjustedRect.width, height: adjustedRect.height)
+                        .position(x: adjustedRect.midX, y: adjustedRect.midY)
+                    Text("\(rectData.label) (\(Int(rectData.confidence * 100))%)")
+                        .position(x: adjustedRect.midX, y: adjustedRect.minY - 10)
+                        .foregroundColor(.red)
+                }
             }
         }
     }
+    
+    func adjustRectForView(rect: CGRect, viewSize: CGSize) -> CGRect {
+        let scaleX = viewSize.width / camera.bufferSize.width
+            let scaleY = viewSize.height / camera.bufferSize.height
+            let scaledWidth = rect.width * scaleX
+            let scaledHeight = rect.height * scaleY
+            let scaledX = rect.origin.x * scaleX
+            let scaledY = viewSize.height - (rect.origin.y * scaleY) - scaledHeight
+            
+            return CGRect(x: scaledX, y: scaledY, width: scaledWidth, height: scaledHeight)
+        }
+
+    
 }
 
 #Preview {
