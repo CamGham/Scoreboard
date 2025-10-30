d4e9613db67e7872089a31af50eb7ef399fc3948
Cameron <greayham@gmail.com>
2025-10-30 21:41:43 +1300

    Simplify object detecion requests

--- Scoreboard/Stream/Camera/CameraModel.swift
+++ Scoreboard/Stream/Camera/CameraModel.swift
@@ -42,6 +42,9 @@ final class CameraModel: NSObject, AVCaptureVideoDataOutputSampleBufferDelegate 
         Task {
             await captureService.setOutputDelegate(source: self)
             await visionModel = try ObjectDetector.createDetector()
+            if let visionModel {
+                setupVision(model: visionModel)
+            }
         }
     }
     
@@ -51,104 +54,35 @@ final class CameraModel: NSObject, AVCaptureVideoDataOutputSampleBufferDelegate 
         }
         do {
             try await captureService.start()
-            observeOrientation()
         } catch {
-            
+            print("Failed to start camera")
         }
     }
     
-    let sequenceHandler = VNSequenceRequestHandler()
-    var startingOb: VNDetectedObjectObservation?
-    let requestHandler = VNSequenceRequestHandler()
+    deinit {
+        print("closing")
+    }
     
-    func makeObservations(pixelBuffer: CVImageBuffer) async {
-        Task {
-            let orientation = exifOrientationFromDeviceOrientation()
-            let vnHandler = VNImageRequestHandler(cvPixelBuffer: pixelBuffer, orientation: orientation)
-
-            guard let visionModel else { return }
-            guard canPredict else { return }
-            
-            Task { @MainActor in
-                canPredict = false
-            }
-            previewSource.updateLayer()
-            /// search for new objects every x frames/seconds
-            if self.rects.isEmpty {
-                let objectDetc = VNCoreMLRequest(model: visionModel) { vnReq, error in
-                    if let results = vnReq.results {
-                        print("found \(results.count)")
-                        self.drawVisionRequestResults(results)
-//                        if let ball = self.drawVisionRequestResults(results) {
-//                            self.startingOb = VNRecognizedObjectObservation(boundingBox: ball.boundingBox)
-//                        }
+    func stop() async {
+        await captureService.stop()
+        self.requests.removeAll()
+        self.rects.removeAll()
+    }
+    
+    func setupVision(model: VNCoreMLModel)  {
+        do {
+            let objectRecognition = VNCoreMLRequest(model: model, completionHandler: { (request, error) in
+                Task {
+                    if let results = request.results {
+                        await self.drawVisionRequestResults(results)
                     }
                 }
-                
-                do {
-                    try vnHandler.perform([objectDetc])
-                } catch {
-                    print("object detection failed")
-                }
-                
-            }
-            
-            // track observed objects every frame
-            
-            
+            })
+            objectRecognition.imageCropAndScaleOption = .scaleFill
+            self.requests = [objectRecognition]
+        } catch let error as NSError {
+            print("Model loading went wrong: \(error)")
         }
-        
-        
-        Task {
-            guard !self.observations.isEmpty else {
-                return
-            }
-            var rects = [RectangleData]()
-            var trackingRequests = [VNRequest]()
-            let orientation = exifOrientationFromDeviceOrientation()
-            for inputOb in self.observations {
-                var request = VNTrackObjectRequest(detectedObjectObservation: inputOb.value)
-                request.trackingLevel = .accurate
-                trackingRequests.append(request)
-            }
-            
-            do {
-                try requestHandler.perform(trackingRequests, on: pixelBuffer, orientation: orientation)
-                
-            } catch {
-                print("tracking failed")
-            }
-            
-            for processedRequest in trackingRequests {
-//                print("tacking for \(processedRequest)")
-                guard let results = processedRequest.results else {
-                    print("failed")
-                    continue
-                }
-                
-                guard let observation = results.first as? VNDetectedObjectObservation else {
-                    print("failed")
-                    continue
-                }
-                
-                guard var previousRect = rectangles[observation.uuid] else {
-                    print("Lost a rect")
-                    continue
-                }
-                
-                guard observation.confidence > 0.5 else {
-                    continue
-                }
-                
-                rects.append(RectangleData(id: observation.uuid, rect: VNImageRectForNormalizedRect(observation.boundingBox, Int(bufferSize.width), Int(bufferSize.height)), label: previousRect.label, confidence: observation.confidence, colour: previousRect.colour))
-                self.observations[observation.uuid] = observation
-            }
-            
-            Task { @MainActor in
-                self.rects = rects
-            }
-        }
-        
     }
     
     
@@ -168,41 +102,34 @@ final class CameraModel: NSObject, AVCaptureVideoDataOutputSampleBufferDelegate 
         let players = observations.filter { ob in
             ob.labels.first?.identifier == "person"
         }
+    }
+    
+    func makeObservations(pixelBuffer: CVImageBuffer) throws {
+        let orientation = exifOrientationFromDeviceOrientation()
+        let vnHandler = VNImageRequestHandler(cvPixelBuffer: pixelBuffer, orientation: orientation)
+
+        guard let visionModel else { return }
+        guard canPredict else { return }
         
-        // make func
-        var potentailPlayersWithBall = [VNRecognizedObjectObservation]()
-        for player in players {
-            if let ball, ball.boundingBox.intersects(player.boundingBox) {
-                potentailPlayersWithBall.append(player)
+        try vnHandler.perform(requests)
+    }
+    
+    func drawVisionRequestResults(_ results: [Any]) async {
+        rects.removeAll()
+        for observation in results where observation is VNRecognizedObjectObservation {
+            guard let objectObservation = observation as? VNRecognizedObjectObservation else {
+                continue
             }
-        }
-        
-        
-        potentailPlayersWithBall.forEach { player in
-            tempRects.append(RectangleData(id: player.uuid, rect: VNImageRectForNormalizedRect(player.boundingBox, Int(bufferSize.width), Int(bufferSize.height)), label: player.labels[0].identifier, confidence: player.confidence, colour: .green))
-        }
-        
-        if let ball {
-            var inputObservation = VNDetectedObjectObservation(boundingBox: ball.boundingBox)
-            let request = VNTrackObjectRequest(detectedObjectObservation: inputObservation)
-            tempRects.append(RectangleData(id: ball.uuid, rect: VNImageRectForNormalizedRect(ball.boundingBox, Int(bufferSize.width), Int(bufferSize.height)), label: ball.labels[0].identifier, confidence: ball.confidence, colour: potentailPlayersWithBall.isEmpty ? .red : .green))
-            
-            
-        }
-        
-        let restOfPlayers = players.filter { p in
-            !potentailPlayersWithBall.contains(p)
-        }
-        
-        tempRects.append(contentsOf: restOfPlayers.map({ ob in
-            RectangleData(id: ob.uuid, rect: VNImageRectForNormalizedRect(ob.boundingBox, Int(bufferSize.width), Int(bufferSize.height)), label: ob.labels[0].identifier, confidence: ob.confidence, colour: .red)
-        }))
-        
-        
-        for rect in tempRects {
-            let inputObservation = VNDetectedObjectObservation(boundingBox: rect.rect)
-            self.observations[inputObservation.uuid] = inputObservation
-            self.rectangles[inputObservation.uuid] = rect
+            // Select only the label with the highest confidence.
+            let topLabelObservation = objectObservation.labels[0]
+            rects.append(
+                RectangleData(
+                    id: UUID(),
+                    rect: objectObservation.boundingBox,
+                    label: topLabelObservation.identifier,
+                    confidence: objectObservation.confidence,
+                    colour: Color.red)
+            )
         }
         
 
@@ -222,7 +149,7 @@ final class CameraModel: NSObject, AVCaptureVideoDataOutputSampleBufferDelegate 
         case UIDeviceOrientation.portraitUpsideDown:  // Device oriented vertically, home button on the top
             exifOrientation = .left
         case UIDeviceOrientation.landscapeLeft:       // Device oriented horizontally, home button on the right
-            exifOrientation = .upMirrored
+            exifOrientation = .up
         case UIDeviceOrientation.landscapeRight:      // Device oriented horizontally, home button on the left
             exifOrientation = .down
         case UIDeviceOrientation.portrait:            // Device oriented vertically, home button on the bottom
@@ -236,8 +163,10 @@ final class CameraModel: NSObject, AVCaptureVideoDataOutputSampleBufferDelegate 
     func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
         guard let buf = sampleBuffer.imageBuffer else { return }
 
-        Task {
-            await makeObservations(pixelBuffer: buf)
+        do {
+            try makeObservations(pixelBuffer: buf)
+        } catch {
+            print("Failed to make observations")
         }
     }
 }
