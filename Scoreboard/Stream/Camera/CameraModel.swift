34fa5eca9de377b1a77f170ac547219203229d68
Cameron <greayham@gmail.com>
2025-11-09 13:11:34 +1300

    Perform best match merge using cost matrix and hungarian alg

--- Scoreboard/Stream/Camera/CameraModel.swift
+++ Scoreboard/Stream/Camera/CameraModel.swift
@@ -71,81 +71,119 @@ final class CameraModel: NSObject, AVCaptureVideoDataOutputSampleBufferDelegate 
         self.rects.removeAll()
     }
     
-    func setupVision(model: VNCoreMLModel)  {
-        do {
-            // === Setup recognition request for objects defined by input model ===
-            // pixel buffer stream camera causes a new VNImageRequestHandler to be created
-            // this runs the following completionHander code every 10 sec that:
-            //      Initiates a new array of tracking requests + UI draws on every object detected
-            let objectRecognition = VNCoreMLRequest(model: model, completionHandler: { (request, error) in
-                // Clear any previous 'initial' object detections
-                Task { @MainActor in
-                    self.rects.removeAll()
+    func setupVision(model: VNCoreMLModel) {
+        
+        // === Setup recognition request for objects defined by input model ===
+        // pixel buffer stream camera causes a new VNImageRequestHandler to be created
+        // this runs the following completionHander code every 10 sec that:
+        //      Initiates a new array of tracking requests + UI draws on every object detected
+        let objectRecognition = VNCoreMLRequest(model: model, completionHandler: { (request, error) in
+            // Clear any previous 'initial' object detections
+            Task { @MainActor in
+                self.rects.removeAll()
+            }
+            
+            if let results = request.results as? [VNRecognizedObjectObservation] {
+                // filter minimum confidence
+                let newObservations = results.filter { objDet in
+                    objDet.confidence > 0.7
+                }
+                guard !newObservations.isEmpty else {
+                    return
                 }
                 
-                if let results = request.results as? [VNRecognizedObjectObservation] {
-                    
-                    // prepare new tracking requests (combine existing with new)
-                    var outputTrackingRequests = [VNTrackObjectRequest]()
-                    
-                    var existingTracks = self.trackingRequests
-                    for track in existingTracks {
-                        guard let trackReq = track as? VNTrackObjectRequest else {
-                            continue
-                        }
-                        
-                        // iou match
-                        // greedily first match at confidence threshold
-                        if let overlapMatch = results.first(where: { newOb in
-                            self.iou(box1: trackReq.inputObservation.boundingBox, box2: newOb.boundingBox) > 0.7
-                        }) {
-                            // updating existing tracking request
-                            trackReq.inputObservation = overlapMatch
-                            outputTrackingRequests.append(trackReq)
-                            
-                        } else if let existingTracking = trackReq.results?.first as? VNDetectedObjectObservation, existingTracking.confidence > 0.5 {
-                            // keep high confidence tracks
-                            outputTrackingRequests.append(trackReq)
-                            
-                        }
-                    }
-                        
-                    for newObservation in results {
-                        let isNew = !outputTrackingRequests.contains { trackRequest in
-                            self.iou(box1: trackRequest.inputObservation.boundingBox, box2: newObservation.boundingBox) > 0.7
-                        }
-                        
-                        if isNew {
-                            // make request for tracking on this observation
-                            let trackRequest = VNTrackObjectRequest(detectedObjectObservation: newObservation)
-                            trackRequest.trackingLevel = .accurate
-                            outputTrackingRequests.append(trackRequest)
-                            
-                            // on UI update initial bounding boxes
-                            Task { @MainActor in
-                                self.rects.append(
-                                    RectangleData(
-                                        id: UUID(),
-                                        rect: newObservation.boundingBox,
-                                        label: newObservation.labels[0].identifier,
-                                        confidence: newObservation.confidence,
-                                        colour: Color.red)
-                                    )
+                // prepare new tracking requests (combine existing with new)
+                var outputTrackingRequests = [VNTrackObjectRequest]()
+                
+                var existingTracks = self.trackingRequests
+                if existingTracks.isEmpty { // make initial tracking requests
+                    self.createNewTrackingRequests(
+                        newObservations: newObservations,
+                        &outputTrackingRequests)
+                    self.trackingRequests = outputTrackingRequests
+                    return
+                }
+                
+                do {
+                    // create cost matrix of exsitingTracks against new observations
+                    let vectorRows: [Vector] = existingTracks.map { trackReq in
+                        return Vector(
+                            newObservations.map { objectDetection in
+                                let overlap = self.iou(
+                                    box1: trackReq.inputObservation.boundingBox,
+                                    box2: objectDetection.boundingBox
+                                )
+                                return 1.0 - overlap // lowest cost
                             }
+                        )
+                    }
+                    let costMatrix: Matrix = Matrix(vectorRows)
+                    
+                    // find best matches using Hungarian algorithm
+                    let assignments = try HungarianAlgorithm.findOptimalAssignment(costMatrix)
+                    
+                    // merge best new observations with existing tracks
+                    for index in assignments.rowIndices.indices {
+                        // get the assignment indicies
+                        let trackIndex = assignments.rowIndices[index]
+                        let bestObservationIndex = assignments.columnIndices[index]
+                        
+                        let track = existingTracks[trackIndex]
+                        let bestObservation = newObservations[bestObservationIndex]
+                        track.inputObservation = bestObservation
+                        outputTrackingRequests.append(track)
+                    }
+                    
+                    // if existingTracks.count > newObservations.count we will have left over tracking requests
+                    // continue any remaining tracks
+                    for trackReq in existingTracks{
+                        if !outputTrackingRequests.contains(where:
+                            {$0.inputObservation.uuid == trackReq.inputObservation.uuid}) {
+                            outputTrackingRequests.append(trackReq)
                         }
                     }
+                    
+                    // if existingTracks.count < newObservations.count we will have left over new observationd
+                    // create new tracking from remainging observations
+                    let remainingObservations = newObservations.filter { ob in
+                        return !outputTrackingRequests.contains { outputReq in
+                            outputReq.inputObservation.uuid == ob.uuid
+                        }
+                    }
+                    self.createNewTrackingRequests(newObservations: remainingObservations, &outputTrackingRequests)
                     
                     self.trackingRequests = outputTrackingRequests
+                } catch {
+                    print("Error occured during tracking merge")
                 }
-            })
-            objectRecognition.imageCropAndScaleOption = .scaleFill
-            self.requests = [objectRecognition]
-        } catch let error as NSError {
-            print("Model loading went wrong: \(error)")
+            }
+        })
+        objectRecognition.imageCropAndScaleOption = VNImageCropAndScaleOption.scaleFill
+        self.requests = [objectRecognition]
+    }
+    
+    private func createNewTrackingRequests(newObservations: [VNRecognizedObjectObservation], _ outputTrackingRequests: inout [VNTrackObjectRequest]) {
+        for o in newObservations {
+            // make request for tracking on this observation
+            let trackRequest = VNTrackObjectRequest(detectedObjectObservation: o)
+            trackRequest.trackingLevel = .accurate
+            outputTrackingRequests.append(trackRequest)
+            
+            // on UI update initial bounding boxes
+            Task { @MainActor in
+                self.rects.append(
+                    RectangleData(
+                        id: UUID(),
+                        rect: o.boundingBox,
+                        label: o.labels[0].identifier,
+                        confidence: o.confidence,
+                        colour: Color.red)
+                )
+            }
         }
     }
     
-    func iou(box1: CGRect, box2: CGRect) -> Double {
+    private func iou(box1: CGRect, box2: CGRect) -> Double {
         // IoU = area of overlap / area of union
         let intersectionRect = box1.intersection(box2)
         if intersectionRect.isNull || intersectionRect.width <= 0 || intersectionRect.height <= 0 {
